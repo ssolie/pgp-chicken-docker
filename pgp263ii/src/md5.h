b1dd9754d219b556cde6692f09678221632ce6fb
steven <steven.solie@dedikey.com>
2021-10-18 20:44:09 -0600

    add pgp src

---
+++ pgp263ii/src/md5.h
@@ -0,0 +1,32 @@
+#ifndef MD5_H
+#define MD5_H
+
+#if defined(__STDC__) && __STDC_VERSION__ >= 199901
+#include <stdint.h>
+typedef uint32_t uint32;
+#else
+#ifdef __alpha
+typedef unsigned int uint32;
+#else
+typedef unsigned long uint32;
+#endif
+#endif /* __STDC__ && __STDC_VERSION__ */
+
+struct MD5Context {
+	uint32 buf[4];
+	uint32 bits[2];
+	unsigned char in[64];
+};
+
+void MD5Init(struct MD5Context *context);
+void MD5Update(struct MD5Context *context, unsigned char const *buf,
+	       unsigned len);
+void MD5Final(unsigned char digest[16], struct MD5Context *context);
+void MD5Transform(uint32 buf[4], uint32 const in[16]);
+
+/*
+ * This is needed to make RSAREF happy on some MS-DOS compilers.
+ */
+typedef struct MD5Context MD5_CTX;
+
+#endif /* !MD5_H */
